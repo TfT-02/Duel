24d3c52f966c228eb0cda3c2905e83827c9f07b8
TfT_02 <tft_02@hotmail.com>
2013-03-31 17:27:27 +0200

    Add more basic setup

---
+++ README.md
@@ -0,0 +1,22 @@
+[![Duel][Banner]][GitHub]
+What is Duel?
+----------------
+
+Duel is a Bukkit plugin that allows two players to challenge each other to a duel.
+Other players will not be allowed to interfere with dueling players.
+
+How does it work?
+----------------
+
+Check out the [BukkitDev]-page for more information!
+
+
+Who made this?
+----------------   
+[![TfT_02](http://www.gravatar.com/avatar/b8914f9970e1f6ffd5281ce4770e20a7.png)](http://dev.bukkit.org/profiles/TfT_02/) 
+
+Download here: [BukkitDev]
+
+[Banner]: https://dl.dropbox.com/u/29178507/Dev/Duel/title-banner.png
+[BukkitDev]: http://dev.bukkit.org/server-mods/duel/
+[GitHub]: https://github.com/TfT-02/Duel
---
+++ src/com/me/tft_02/duel/util/UpdateChecker.java
@@ -0,0 +1,67 @@
+package com.me.tft_02.duel.util;
+
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.me.tft_02.duel.Duel;
+
+public class UpdateChecker {
+    static Duel plugin;
+
+    private UpdateChecker() {}
+
+    public static boolean updateAvailable() throws Exception {
+        String version = Duel.getInstance().getDescription().getVersion();
+        URL url = new URL("http://api.bukget.org/api2/bukkit/plugin/duel/latest");
+        InputStreamReader isr;
+        try {
+            isr = new InputStreamReader(url.openStream());
+        }
+        catch (UnknownHostException e) {
+            return false;
+        }
+
+        String newVersion;
+        try {
+            JSONParser jp = new JSONParser();
+            Object o = jp.parse(isr);
+
+            if (!(o instanceof JSONObject)) {
+                isr.close();
+                return false;
+            }
+
+            JSONObject jo = (JSONObject) o;
+            jo = (JSONObject) jo.get("versions");
+            newVersion = (String) jo.get("version");
+
+            String[] oldTokens = version.split("[.]");
+            String[] newTokens = newVersion.split("[.]");
+
+            for (int i = 0; i < 3; i++) {
+                Integer newVer = Integer.parseInt(newTokens[i]);
+                Integer oldVer;
+                try {
+                    oldVer = Integer.parseInt(oldTokens[i]);
+                }
+                catch (NumberFormatException e) {
+                    oldVer = 0;
+                }
+                if (oldVer < newVer) {
+                    isr.close();
+                    return true;
+                }
+            }
+            return false;
+        }
+        catch (ParseException e) {
+            isr.close();
+            return false;
+        }
+    }
+}
