dc46ae60c75ac5ac0f8984a1f39d70a971290805
TfT_02 <tft_02@hotmail.com>
2013-04-27 15:54:55 +0200

    Make the stats command prettier

--- src/com/me/tft_02/duel/commands/StatsCommand.java
+++ src/com/me/tft_02/duel/commands/StatsCommand.java
@@ -1,5 +1,8 @@
 package com.me.tft_02.duel.commands;
 
+import java.text.DecimalFormat;
+
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,19 +12,38 @@ import com.me.tft_02.duel.database.DatabaseManager;
 
 public class StatsCommand implements CommandExecutor {
 
+    protected DecimalFormat decimal = new DecimalFormat("##0.00");
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage("sorry!");
+            sender.sendMessage("You cannot use this command from the console!");
         }
 
         Player player = (Player) sender;
+        int wins = DatabaseManager.getWinCount(player);
+        int losses = DatabaseManager.getLossCount(player);
+        int ties = DatabaseManager.getTieCount(player);
+        String ratio = calculateRatio(wins, losses);
 
-        player.sendMessage("STATS");
+        String border = ChatColor.GOLD + "|";
 
-        player.sendMessage("WINS: " + DatabaseManager.getWinCount(player));
-        player.sendMessage("LOSSES: " + DatabaseManager.getLossCount(player));
-        player.sendMessage("TIES: " + DatabaseManager.getTieCount(player));
+        player.sendMessage(border + ChatColor.GOLD + "----[ " + ChatColor.DARK_RED + "Duel Stats" + ChatColor.GOLD + " ]----- ");
+        player.sendMessage(border + ChatColor.DARK_AQUA + "  WINS: " + ChatColor.GRAY + wins);
+        player.sendMessage(border + ChatColor.DARK_AQUA + "  LOSSES: " + ChatColor.GRAY + losses);
+        player.sendMessage(border + ChatColor.DARK_AQUA + "  TIES: " + ChatColor.GRAY + ties);
+        player.sendMessage(border + ChatColor.GOLD + "--------------------");
+        player.sendMessage(border + ChatColor.DARK_AQUA + "  W/L RATIO: " + ChatColor.GRAY + ratio);
+        player.sendMessage(border + ChatColor.GOLD + "--------------------");
         return true;
+    }
+
+    private String calculateRatio(int first, int second) {
+        double ratio = 0.0;
+        if (second > 0) {
+            ratio = first / second;
+        }
+
+        return decimal.format(ratio);
     }
 }
