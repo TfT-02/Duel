5828243b8b4d8a95b07cdc671fdbb5797202af7a
TfT_02 <tft_02@hotmail.com>
2013-04-29 23:00:37 +0200

    Convert to a Maven project!

--- src/main/java/com/me/tft_02/duel/Duel.java
+++ src/main/java/com/me/tft_02/duel/Duel.java
@@ -5,7 +5,6 @@ import java.util.logging.Level;
 
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
@@ -17,7 +16,6 @@ import com.me.tft_02.duel.listeners.PlayerListener;
 import com.me.tft_02.duel.runnables.DuelRangeTask;
 import com.me.tft_02.duel.util.Metrics;
 import com.me.tft_02.duel.util.UpdateChecker;
-import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 
 public class Duel extends JavaPlugin {
     public static Duel instance;
@@ -45,8 +43,6 @@ public class Duel extends JavaPlugin {
         pm.registerEvents(entityListener, this);
 
         setupConfiguration();
-
-        setupWorldGuard();
 
         getCommand("duel").setExecutor(new DuelCommand());
 
@@ -88,23 +84,6 @@ public class Duel extends JavaPlugin {
 
         config.options().copyDefaults(true);
         saveConfig();
-    }
-
-    private void setupWorldGuard() {
-        if (getServer().getPluginManager().isPluginEnabled("WorldGuard")) {
-            worldGuardEnabled = true;
-            getLogger().info("WorldGuard found!");
-        }
-    }
-
-    WorldGuardPlugin getWorldGuard() {
-        Plugin plugin = getServer().getPluginManager().getPlugin("WorldGuard");
-
-        if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
-            return null;
-        }
-
-        return (WorldGuardPlugin) plugin;
     }
 
     /**
