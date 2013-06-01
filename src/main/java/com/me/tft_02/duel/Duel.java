392760aaf8608d2ce16bc01000d95c92c35e1d41
TfT_02 <tft_02@hotmail.com>
2013-06-01 15:57:11 +0200

    Add WorldGuard support so we can block the prevent PVP messages

--- src/main/java/com/me/tft_02/duel/Duel.java
+++ src/main/java/com/me/tft_02/duel/Duel.java
@@ -5,23 +5,30 @@ import java.util.logging.Level;
 
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
 import com.me.tft_02.duel.commands.DuelCommand;
 import com.me.tft_02.duel.database.Data;
+import com.me.tft_02.duel.hooks.WorldGuardListener;
 import com.me.tft_02.duel.listeners.EntityListener;
 import com.me.tft_02.duel.listeners.PlayerListener;
 import com.me.tft_02.duel.runnables.DuelRangeTask;
 import com.me.tft_02.duel.util.Metrics;
 import com.me.tft_02.duel.util.UpdateChecker;
 
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+
 public class Duel extends JavaPlugin {
     public static Duel instance;
 
     private final PlayerListener playerListener = new PlayerListener(this);
     private final EntityListener entityListener = new EntityListener(this);
+
+    // Listeners of dependencies
+    private WorldGuardListener worldGuardListener = new WorldGuardListener(this);
 
     public boolean worldGuardEnabled = false;
 
@@ -43,6 +50,8 @@ public class Duel extends JavaPlugin {
         pm.registerEvents(entityListener, this);
 
         setupConfiguration();
+
+        setupWorldGuard();
 
         getCommand("duel").setExecutor(new DuelCommand());
 
@@ -84,6 +93,24 @@ public class Duel extends JavaPlugin {
 
         config.options().copyDefaults(true);
         saveConfig();
+    }
+
+    private void setupWorldGuard() {
+        if (getServer().getPluginManager().isPluginEnabled("WorldGuard")) {
+            worldGuardEnabled = true;
+            getLogger().info("WorldGuard found!");
+            getServer().getPluginManager().registerEvents(worldGuardListener, this);
+        }
+    }
+
+    WorldGuardPlugin getWorldGuard() {
+        Plugin plugin = getServer().getPluginManager().getPlugin("WorldGuard");
+
+        if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
+            return null;
+        }
+
+        return (WorldGuardPlugin) plugin;
     }
 
     /**
---
+++ src/main/java/com/me/tft_02/duel/hooks/WorldGuardListener.java
@@ -0,0 +1,34 @@
+package com.me.tft_02.duel.hooks;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+import com.me.tft_02.duel.Config;
+import com.me.tft_02.duel.Duel;
+import com.me.tft_02.duel.datatypes.player.PlayerData;
+import com.sk89q.worldguard.protection.events.DisallowedPVPEvent;
+
+public class WorldGuardListener implements Listener {
+    Duel plugin;
+
+    public WorldGuardListener(final Duel instance) {
+        plugin = instance;
+    }
+
+    /**
+     * Handle DisallowedPVPEvent events that involve modifying the event.
+     *
+     * @param event The event to modify
+     */
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onDisallowedPVP(DisallowedPVPEvent event) {
+        Player attacker = event.getAttacker();
+        Player defender = event.getDefender();
+
+        if (Config.getOverridePVP() && PlayerData.areDueling(attacker, defender)) {
+            event.setCancelled(false);
+        }
+    }
+}
