1bc2d6872dfa7609c1285dc4140891761a03b7d0
TfT_02 <tft_02@hotmail.com>
2013-08-02 23:00:14 +0200

    Remove extra whitespaces

--- src/main/java/com/me/tft_02/duel/config/Config.java
+++ src/main/java/com/me/tft_02/duel/config/Config.java
@@ -37,21 +37,21 @@ public class Config extends AutoUpdateConfigLoader {
     public boolean getConfigOverwriteEnabled() { return config.getBoolean("General.Config_Update_Overwrite", true); }
 
     /* DUEL SETTINGS */
-    public  boolean getPreventPVP() { return config.getBoolean("Duel.Prevent_PVP", false); }
-    public  boolean getOverridePVP() { return config.getBoolean("Duel.Override_PVP", true); }
-    public  boolean getSaveInventory() { return config.getBoolean("Duel.Save_Inventory", true); }
-    public  int getDuelLength() { return config.getInt("Duel.Duel_Length", 120); }
-    public  int getMessageRange() { return config.getInt("Duel.Message_Range", 100); }
-    public  boolean getHealEnabled() { return config.getBoolean("Duel.Heal_After_Duel", true); }
+    public boolean getPreventPVP() { return config.getBoolean("Duel.Prevent_PVP", false); }
+    public boolean getOverridePVP() { return config.getBoolean("Duel.Override_PVP", true); }
+    public boolean getSaveInventory() { return config.getBoolean("Duel.Save_Inventory", true); }
+    public int getDuelLength() { return config.getInt("Duel.Duel_Length", 120); }
+    public int getMessageRange() { return config.getInt("Duel.Message_Range", 100); }
+    public boolean getHealEnabled() { return config.getBoolean("Duel.Heal_After_Duel", true); }
 
     /* ARENA SETTINGS */
-    public  boolean getBorderKnockBack() { return config.getBoolean("Arena.Knockback_Enabled", true); }
-    public  double getArenaSize() { return config.getDouble("Arena.Radius", 20.0); }
-    public  boolean getBorderParticleEffectsEnabled() { return false; }
+    public boolean getBorderKnockBack() { return config.getBoolean("Arena.Knockback_Enabled", true); }
+    public double getArenaSize() { return config.getDouble("Arena.Radius", 20.0); }
+    public boolean getBorderParticleEffectsEnabled() { return false; }
 
     /* CHALLENGE SETTINGS */
-    public  int getInviteTimeout() { return config.getInt("Challenge.Timeout", 30); }
-    public  HashSet<Material> getDuelWeaponItems() {
+    public int getInviteTimeout() { return config.getInt("Challenge.Timeout", 30); }
+    public HashSet<Material> getDuelWeaponItems() {
         HashSet<Material> miscItems = new HashSet<Material>();
 
         for (String item : config.getStringList("Challenge.Duel_Items")) {
@@ -65,8 +65,8 @@ public class Config extends AutoUpdateConfigLoader {
     }
 
     /* WORLDGUARD SETTINGS */
-    public  boolean getUseAsWhitelist() { return config.getBoolean("WorldGuard.Use_As_Whitelist", false); }
-    public  List<String> getRegionList() { return Duel.p.getConfig().getStringList("WorldGuard.Regions"); }
+    public boolean getUseAsWhitelist() { return config.getBoolean("WorldGuard.Use_As_Whitelist", false); }
+    public List<String> getRegionList() { return Duel.p.getConfig().getStringList("WorldGuard.Regions"); }
 
     /* @formatter:on */
 }
